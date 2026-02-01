f8dc9fb9cc40e7126f887b4c41fca8937d9418f5
NotWhiskey12138 <137181834+NotWhiskey12138@users.noreply.github.com>
2026-01-31 21:55:54 -0800

    Level-Tutorial v1.0

--- Assets/Scripts/NPC/Frag/SwingPoint.cs
+++ Assets/Scripts/NPC/Frag/SwingPoint.cs
@@ -1,16 +1,24 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class SwingPoint : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Tween idleFloatTween;
+    Tween idleRotateTween;
+
+    public void PlayIdleBuzz()
     {
-        
+        transform.DOKill(); // 防止残留
+
+        // 上下漂浮
+        idleFloatTween = transform.DOMoveY(transform.position.y + 0.08f, 0.5f)
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo);
+
+        // 左右轻微摆头
+        idleRotateTween = transform.DORotate(new Vector3(0, 0, 3f), 0.3f)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.InOutSine);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
